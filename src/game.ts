90126b802d4dd4dec179664aff5c3a5cb01f8225
Jake Klassen <jklassendev@gmail.com>
2019-09-29 10:35:39 -0400

    initial work

--- src/game.ts
+++ src/game.ts
@@ -1,17 +1,22 @@
 import { getResolution } from './lib/screen';
+import { getNeighbours } from './lib/1d-array';
 
-const seed = (arr: number[][], rows = 0, cols = 0) =>
-  (arr[Math.floor(Math.random() * rows)][Math.floor(Math.random() * cols)] =
-    State.Live);
+const seed = (arr: Cell[]) => {
+  const idx = Math.floor(Math.random() * arr.length);
+  arr[idx].state = State.Live;
+
+  return idx;
+};
 
 const GAME_WIDTH = 320;
 const GAME_HEIGHT = 180;
+const SEED_ITERATIONS = 1;
 
 const canvas = document.createElement('canvas') as HTMLCanvasElement;
 canvas.width = GAME_WIDTH;
 canvas.height = GAME_HEIGHT;
 
-enum State {
+export enum State {
   Dead = 0,
   Live = 1,
 }
@@ -40,28 +45,34 @@ resize();
 
 window.addEventListener('resize', resize);
 
-// Store adjacent lookup positions
-const directions = [
-  { x: -1, y: -1 }, // top left
-  { x: -1, y: 0 }, // left
-  { x: -1, y: 1 }, // bottom left
-  { x: 0, y: -1 }, // top
-  { x: 0, y: 1 }, // bottom
-  { x: 1, y: -1 }, // top right
-  { x: 1, y: 0 }, // right
-  { x: 1, y: 1 }, // bottom right
-];
+type Cell = {
+  x: number;
+  y: number;
+  state: State;
+};
 
-const cells = Array.from({ length: GAME_HEIGHT }, () =>
-  new Array(GAME_WIDTH).fill(0),
-);
-const next = Array.from({ length: GAME_HEIGHT }, () =>
-  new Array(GAME_WIDTH).fill(0),
-);
+const cells: Cell[] = [];
+const next: Cell[] = [];
 
-for (let i = 0; i < 5500; ++i) {
-  seed(cells, GAME_HEIGHT, GAME_WIDTH);
+for (let y = 0; y < GAME_HEIGHT; ++y) {
+  for (let x = 0; x < GAME_WIDTH; ++x) {
+    cells.push({
+      x,
+      y,
+      state: State.Dead,
+    });
+
+    next.push({
+      x,
+      y,
+      state: State.Dead,
+    });
+  }
 }
+
+// for (let i = 0; i < SEED_ITERATIONS; ++i) {
+//   rnd = seed(cells);
+// }
 
 let dt = 0;
 let last = performance.now();
@@ -70,58 +81,71 @@ const step = 1 / fps;
 
 console.log("Conway's Game of Life");
 
-function frame(hrt: DOMHighResTimeStamp) {
-  requestAnimationFrame(frame);
+// for (const [idx, { state }] of cells.entries()) {
+//   // console.log(gn(idx, GAME_WIDTH, GAME_HEIGHT));
+//   console.log(idx);
+// }
 
-  dt += (hrt - last) / 1000;
+// function frame(hrt: DOMHighResTimeStamp) {
+//   requestAnimationFrame(frame);
 
-  if (dt > step) {
-    dt = 0;
+//   dt += (hrt - last) / 1000;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
+//   if (dt > step) {
+//     dt = 0;
 
-    for (let y = 0; y < cells.length; ++y) {
-      for (let x = 0; x < cells[y].length; ++x) {
-        const cell = cells[y][x];
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.fillStyle = 'white';
 
-        const liveNeighbourCount = directions.filter(
-          dir => (cells[y + dir.y] || [])[x + dir.x] === State.Live,
-        ).length;
+//     for (const [idx, cell] of cells.entries()) {
+//       const liveNeighbourCount = getNeighbours(
+//         cells,
+//         idx,
+//         GAME_WIDTH,
+//         GAME_HEIGHT,
+//       );
 
-        /**
-         * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-         * Any live cell with two or three live neighbours lives on to the next generation.
-         * Any live cell with more than three live neighbours dies, as if by overpopulation.
-         * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-         */
+//       /**
+//        * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//        * Any live cell with two or three live neighbours lives on to the next generation.
+//        * Any live cell with more than three live neighbours dies, as if by overpopulation.
+//        * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+//        */
 
-        // Write to `next` state
-        if (cell === State.Dead) {
-          if (liveNeighbourCount === 3) {
-            next[y][x] = 1;
-          }
-        } else if (cell === State.Live) {
-          if (liveNeighbourCount < 2 || liveNeighbourCount > 3) {
-            next[y][x] = 0;
-          }
-        }
-      }
-    }
+//       // Write to `next` state
+//       if (cell.state === State.Dead) {
+//         if (liveNeighbourCount === 3) {
+//           next[idx].state = State.Live;
+//         }
+//       } else if (cell.state === State.Live) {
+//         if (liveNeighbourCount < 2 || liveNeighbourCount > 3) {
+//           next[idx].state = State.Dead;
+//         }
+//       }
+//     }
 
-    // Assign next state and draw
-    for (let y = 0; y < next.length; ++y) {
-      for (let x = 0; x < next[y].length; ++x) {
-        cells[y][x] = next[y][x];
+//     // Assign next state and draw
+//     for (const [idx, { x, y, state }] of next.entries()) {
+//       cells[idx] = next[idx];
 
-        if (cells[y][x] === State.Live) {
-          ctx.fillRect(x, y, 1, 1);
-        }
-      }
-    }
-  }
+//       if (state === State.Live) {
+//         ctx.fillRect(x, y, 1, 1);
+//       }
+//     }
+//   }
 
-  last = hrt;
-}
+//   last = hrt;
+// }
 
-requestAnimationFrame(frame);
+// requestAnimationFrame(frame);
+
+setInterval(() => {
+  const rnd = Math.floor(Math.random() * GAME_HEIGHT * GAME_WIDTH);
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(cells[rnd].x, cells[rnd].y, 1, 1);
+  ctx.fillStyle = 'white';
+  getNeighbours(cells, rnd, GAME_WIDTH, GAME_HEIGHT).forEach(
+    idx => cells[idx] && ctx.fillRect(cells[idx].x, cells[idx].y, 1, 1),
+  );
+}, 250);
---
+++ src/lib/1d-array.ts
@@ -0,0 +1,21 @@
+import { State } from '../game';
+
+export const getNeighbours = (
+  cells: { state: State; x: number; y: number }[],
+  idx: number,
+  width: number,
+  height: number,
+  mode = 8,
+) => {
+  return [
+    idx - width, // north
+    idx - width + 1, // northeast
+    idx + 1, // east
+    idx + 1 + width, // southeast
+    idx + width, // south
+    idx - 1 + width, // southwest
+    idx - 1, // west
+    idx - 1 - width, // northwest
+  ];
+  //.filter(idx => cells[idx] != null && cells[idx].state === State.Live).length;
+};
