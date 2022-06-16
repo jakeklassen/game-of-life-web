b1a17bd70c4b2e2be7311c341233af6c00c422eb
Jake Klassen <jklassendev@gmail.com>
2022-06-16 09:28:26 -0400

    comments

--- src/game.ts
+++ src/game.ts
@@ -94,7 +94,7 @@ function frame(hrt: DOMHighResTimeStamp) {
       const liveNeighbourCount = getLiveNeighbourCount(cells, idx);
 
       /**
-       * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+       * Any live cell with fewer than two live neighbours dies, as if by under population.
        * Any live cell with two or three live neighbours lives on to the next generation.
        * Any live cell with more than three live neighbours dies, as if by overpopulation.
        * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
